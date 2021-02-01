924e61257bcdc974a991eccfcc50c79a6520deae
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-02-01 15:44:28 +0100

    removed unused imports for netlify build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import BrainReactCarousel from "./components/BrainReactCarousel";
-import Carousel from "./components/Carousel"; 
-import PureCarousel from "./components/PureCarousel";
-import ResponsiveCarousel from "./components/ResponsiveCarousel";
 import SlickCarousel from "./components/SlickCarousel";
 import styled from "styled-components";
 
